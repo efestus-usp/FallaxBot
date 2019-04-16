6b626dbc7174cfb30c0e559e53aa8cfbe09a4a27
BrenoUSP <brenoslivio@usp.br>
2019-04-16 06:17:28 -0300

    Preparing to join different sentences to create the news headlines

--- NewsGen/WebScraping.py
+++ NewsGen/WebScraping.py
@@ -17,11 +17,13 @@ def get_soup(url, header):
 
 def run():
 
+    text = ""
+
     #BBC
     link1 = "https://www.bbc.com/portuguese/topics/31684f19-84d6-41f6-b033-7ae08098572a"
     html1 = requests.get(link1).text
     soup = BeautifulSoup(html1, 'html.parser')
-    text = ""
+    textBBC = ""
 
     i = 0
     num = random.randint(1, 20)
@@ -29,12 +31,204 @@ def run():
     for p in soup.find_all('h3'):
         i = i + 1
         if num == i:
-            print(p.get_text().rstrip())
-            text = p.get_text().rstrip()
+            textBBC = p.get_text().rstrip()
             break
 
+    #G1
+    link2 = "https://g1.globo.com/economia/tecnologia/"
+    html2 = requests.get(link2).text
+    #print(html2)
+    soup = BeautifulSoup(html2, 'html.parser')
+    textG1 = ""
 
+    i = 0
+    num = random.randint(1, 10)
 
+    for p in soup.find_all("a", {"class": "feed-post-link gui-color-primary gui-color-hover"}):
+        i = i + 1
+        if num == i:
+            textG1 = "\n" + p.get_text().rstrip()
+            break
+
+    #OlharDigital
+    link3 = "https://olhardigital.com.br/noticias/"
+    html3 = requests.get(link3).text
+    soup = BeautifulSoup(html3, 'html.parser')
+    textOD = ""
+
+    i = 0
+    num = random.randint(1, 30)
+
+    for p in soup.find_all("h3"):
+        i = i + 1
+        if num == i:
+            textOD = "\n" + p.get_text().rstrip()
+            break
+
+    beginSelection = random.randint(0, 2)
+
+    if beginSelection == 0:
+        array = textBBC.split()
+        for str in array:
+            verb = False
+            text += str + ' '
+            link = "https://www.dicio.com.br/pesquisa.php?q=" + str
+            html = requests.get(link).text
+            soup = BeautifulSoup(html, 'html.parser')
+
+            for p in soup.find_all("li"):
+                if "verb" in p.get_text():
+                    verb = True
+                    break
+                if "substantivo":
+                    break
+
+            if verb:
+                break
+    elif beginSelection == 1:
+        array = textG1.split()
+        for str in array:
+            verb = False
+            text += str + ' '
+            link = "https://www.dicio.com.br/pesquisa.php?q=" + str
+            html = requests.get(link).text
+            soup = BeautifulSoup(html, 'html.parser')
+
+            for p in soup.find_all("li"):
+                if "verb" in p.get_text():
+                    verb = True
+                    break
+                if "substantivo":
+                    break
+
+            if verb:
+                break
+    elif beginSelection == 2:
+        array = textOD.split()
+        for str in array:
+            verb = False
+            text += str + ' '
+            link = "https://www.dicio.com.br/pesquisa.php?q=" + str
+            html = requests.get(link).text
+            soup = BeautifulSoup(html, 'html.parser')
+
+            for p in soup.find_all("li"):
+                if "verb" in p.get_text():
+                    verb = True
+                    break
+                if "substantivo":
+                    break
+
+            if verb:
+                break
+
+    print(text)
+
+    #BBC
+    link1 = "https://www.bbc.com/portuguese/topics/31684f19-84d6-41f6-b033-7ae08098572a"
+    html1 = requests.get(link1).text
+    soup = BeautifulSoup(html1, 'html.parser')
+    textBBC = ""
+
+    i = 0
+    num = random.randint(1, 20)
+
+    for p in soup.find_all('h3'):
+        i = i + 1
+        if num == i:
+            textBBC = p.get_text().rstrip()
+            break
+
+    #G1
+    link2 = "https://g1.globo.com/economia/tecnologia/"
+    html2 = requests.get(link2).text
+    #print(html2)
+    soup = BeautifulSoup(html2, 'html.parser')
+    textG1 = ""
+
+    i = 0
+    num = random.randint(1, 10)
+
+    for p in soup.find_all("a", {"class": "feed-post-link gui-color-primary gui-color-hover"}):
+        i = i + 1
+        if num == i:
+            textG1 = "\n" + p.get_text().rstrip()
+            break
+
+    #OlharDigital
+    link3 = "https://olhardigital.com.br/noticias/"
+    html3 = requests.get(link3).text
+    soup = BeautifulSoup(html3, 'html.parser')
+    textOD = ""
+
+    i = 0
+    num = random.randint(1, 30)
+
+    for p in soup.find_all("h3"):
+        i = i + 1
+        if num == i:
+            textOD = "\n" + p.get_text().rstrip()
+            break
+
+    ## GET ANOTHER SENTENCE TO SUM
+
+    endSelection = random.randint(0, 2)
+    verb = False
+
+    if endSelection == 0:
+        array = textBBC.split()
+        for strlst in array:
+
+            if verb:
+                text += strlst + ' '
+            else:
+                link = "https://www.dicio.com.br/pesquisa.php?q=" + strlst
+                html = requests.get(link).text
+                soup = BeautifulSoup(html, 'html.parser')
+
+                for p in soup.find_all("li"):
+                    if "verb" in p.get_text():
+                        verb = True
+                        break
+                    if "substantivo":
+                        break
+    elif endSelection == 1:
+        array = textG1.split()
+        for strlst in array:
+
+            if verb:
+                text += strlst + ' '
+            else:
+                link = "https://www.dicio.com.br/pesquisa.php?q=" + strlst
+                html = requests.get(link).text
+                soup = BeautifulSoup(html, 'html.parser')
+
+                for p in soup.find_all("li"):
+                    if "verb" in p.get_text():
+                        verb = True
+                        break
+                    if "substantivo":
+                        break
+    elif endSelection == 2:
+        array = textOD.split()
+        for strlst in array:
+
+            if verb:
+                text += strlst + ' '
+            else:
+                link = "https://www.dicio.com.br/pesquisa.php?q=" + strlst
+                html = requests.get(link).text
+                soup = BeautifulSoup(html, 'html.parser')
+
+                for p in soup.find_all("li"):
+                    if "verb" in p.get_text():
+                        verb = True
+                        break
+                    if "substantivo":
+                        break
+
+    print(text)
+    text = "\n" + text
 
     query = text.encode('ascii', 'ignore').decode('ascii')
     image_type = "ActiOn"
@@ -52,12 +246,14 @@ def run():
         ActualImages.append((link, Type))
 
     ###print images
+    j = random.randint(0, 1)
 
-    j = random.randint(0, 10)
     for i, (img, Type) in enumerate(ActualImages):
         if i == j:
             break
 
-    urllib2.urlretrieve(img, "Source.jpg")
+    opener = urllib2.FancyURLopener({})
+    opener.version = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.69 Safari/537.36'
+    opener.retrieve(img, 'Source.jpg')
 
     return text
