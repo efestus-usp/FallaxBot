8e433663f811db6edcce8649273bcd49a969491a
breno.almeida <brenoslivio@usp.br>
2019-04-22 10:44:59 -0300

    RNN using RNN Size 500

--- NewsGen/Facebook.py
+++ NewsGen/Facebook.py
@@ -1,6 +1,6 @@
 import facebook
 
-def postFacebook(text):
-    page_access_token = "token"
+def postFacebook():
+    page_access_token = ""
     graph = facebook.GraphAPI(page_access_token)
-    graph.put_photo(image=open("Result.png",'rb'), message=text)
+    graph.put_photo(image=open("Result.png",'rb'), message="Teste")
